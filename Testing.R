3d863d008eaf0d3757685daf65e8b7553441c9be
sgideon1 <sophie_gideon@brown.edu>
2025-11-25 22:23:47 -0500

    Main and helper tests

--- Testing.R
+++ Testing.R
@@ -1,10 +1,9 @@
 
-# MAIN TESTS: 
+# MAIN TESTS
 
-## Estimation
+### Estimation
 
 ## estimate_arrival_rates test
-
 estimation_sim <- data.frame(
   start_time = c("2024-01-01 07:00:00", "2024-01-02 10:00:00"),
   end_time = c("2024-01-01 07:30:00", "2024-01-02 10:30:00"),
@@ -13,8 +12,9 @@ estimation_sim <- data.frame(
 
 estimate_arrival_rates(estimation_sim)
 
-## Simulation
+### Simulation
 
+## Sim_full_day
 full_day_test <- data.frame( # test full day simulation, only includes stations 1-3
   start_station = c(1,1,1,2,2,2),
   end_station = c(2,2,2,3,3,3),
@@ -28,14 +28,14 @@ head(sim_test[, c("origin","destination","clock")])
 
 # UTILITY/HELPER TESTS
 
-# Estimation
+### Estimation
 
 ## Extract_hour
 Extract_hour(arrival_rates, 23, 21) # test
 Extract_hour(arrival_rates, 12, 7) # test
 Extract_hour(arrival_rates, 7, 11) # test
 
-## Sim_station_pairs
+### Sim_station_pairs
 Sim_station_pair(rep(0, 24), "A", "B") # test for sim_station_pair
 Sim_station_pair(rep(1, 24), "A", "B")  # test for sim_station_pair
   # (within sim_station pairs) Testing lambda max
